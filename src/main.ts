2e61dd64c7093c65370eb97746b10bf076531da3
iWill <alxs1dd93@gmail.com>
2026-08-29 19:35:29 +0700

    fix + docker

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,11 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
+  app.enableCors({
+    origin: ['http://localhost:4200'],
+    methods: 'GET,POST,PUT,DELETE,PATCH',
+    credentials: true,
+  });
 
   app.useGlobalPipes(new ValidationPipe());
 
